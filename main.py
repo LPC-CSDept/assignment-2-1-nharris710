29dcdcc05da555eb74a7fe2b6b51605c5cce86c7
Noah Harris <108839766+nharris710@users.noreply.github.com>
2023-02-12 01:59:52 -0800

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ def main():
 
 if __name__ == '__main__':
     main()
-    numM =int(input('enter the number of males:'))
-    numF =int(input('enter the number of females:'))
+numM =int(input('enter the number of males:'))
+numF =int(input('enter the number of females:'))
 
-    perceM = numM /(numM + numF) * 100
-    perceF = numF /(numM + numF) * 100
+perceM = numM /(numM + numF) * 100
+perceF = numF /(numM + numF) * 100
 # Output 
-    print ('total number of students: ',numM + numF)
-    print ('Number of males and females', numM, numF)
+print ('total number of students: ',numM + numF)
+print ('Number of males and females', numM, numF)
 #print ("percentage of males and females")
-    print('The percentage of males and females',perceM,perceF)
+print('The percentage of males and females',perceM,perceF)
