15776a3ba91072747c2dd48081f8fb6265baf7aa
Noah Harris <108839766+nharris710@users.noreply.github.com>
2023-02-15 01:51:23 +0000

    female to male

--- main.py
+++ main.py
@@ -2,12 +2,7 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
-    pass
-
-
-if __name__ == '__main__':
-    main()
-    numM =int(input('enter the number of males:'))
+     numM =int(input('enter the number of males:'))
     numF =int(input('enter the number of females:'))
 
     perceM = numM /(numM + numF) * 100
@@ -17,3 +12,10 @@ if __name__ == '__main__':
     print ('Number of males and females', numM, numF)
 #print ("percentage of males and females")
     print('The percentage of males and females',perceM,perceF)
+
+    pass
+
+
+if __name__ == '__main__':
+    main()
+  
